789b93e56376d322c5a90d2dece442672d85b318
Hugo Mercado <hugo.mario.mercado@gmail.com>
2021-05-30 08:49:22 -0500

    added response

--- cwf-server/app.py
+++ cwf-server/app.py
@@ -1,13 +1,47 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request, make_response
 import chevron
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    print("" + request.args.get(""))
-    return 'Hello World!'
+@app.route('/', methods=['POST', 'OPTIONS'])
+def generate():
+
+    if request.method == "OPTIONS":
+        response = make_response()
+        add_corss(response)
+        return response
+
+    content = request.json
+
+    branches = list()
+    if content["masterBranch"] == "true":
+        branches.append("master")
+    if content["testBranch"] == "true":
+        branches.append("test-framework")
+    if content["threeBranch"] == "true":
+        branches.append("3.0.0")
+
+    content["branch"] = branches
+
+    output = ""
+    with open('workflow.mustache', 'r') as template:
+        output = chevron.render(template, content)
+
+    print(output)
+    data = dict()
+    data["output"] = output
+
+    response = jsonify(data)
+    add_corss(response)
+
+    return response
+
+
+def add_corss(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
 
 
 if __name__ == '__main__':
